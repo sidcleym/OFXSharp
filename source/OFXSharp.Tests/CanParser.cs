36c5e4a23becc8289fbedb0ea20442682a882f8f
Antonio Milesi Bastos <antonio@milesibastos.com>
2014-03-06 14:58:17 -0300

    workaround to search values on root node

--- source/OFXSharp.Tests/CanParser.cs
+++ source/OFXSharp.Tests/CanParser.cs
@@ -4,21 +4,33 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace OFXSharp.Tests
 {
+    [TestFixture]
     public class CanParser
     {
+        [Test]
         public void CanParserItau()
         {
             var parser = new OFXDocumentParser();
             var ofxDocument = parser.Import(new FileStream(@"itau.ofx", FileMode.Open));
+
+            Assert.AreEqual(ofxDocument.Account.AccountID, "9999 99999-9");
+            Assert.AreEqual(ofxDocument.Account.BankID, "0341");
+
+            Assert.AreEqual(3, ofxDocument.Transactions.Count());
+            CollectionAssert.AreEqual(ofxDocument.Transactions.Select(x => x.Memo.Trim()).ToList(), new[] { "RSHOP", "REND PAGO APLIC AUT MAIS", "SISDEB" });
         }
 
+        [Test]
         public void CanParserSantander()
         {
             var parser = new OFXDocumentParser();
             var ofxDocument = parser.Import(new FileStream(@"santander.ofx", FileMode.Open));
+
+            Assert.IsNotNull(ofxDocument);
         }
     }
 }
